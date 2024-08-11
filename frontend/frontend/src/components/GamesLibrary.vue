713cd4b5f6ebb3c011da2b5ab7c6fe9bdaab0c06
Shameer Rehman <shameerrehman@Shameers-MacBook-Air.local>
2024-08-11 13:22:38 -0400

    adding frontend games table display

---
+++ frontend/frontend/src/components/GamesLibrary.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="jumbotron vertical-center">
+    <div class="container">
+      <!-- bootswatch cdn -->
+      <link
+        rel="stylesheet"
+        href="https://cdn.jsdelivr.net/npm/bootswatch@4.5.2/dist/sketchy/bootstrap.min.css"
+        integrity="sha384-RxqHG2ilm4r6aFRpGmBbGTjsqwfqHOKy1ArsMhHusnRO47jcGqpIQqlQK/kmGy9R"
+        crossorigin="anonymous"
+      />
+      <div class="row">
+        <div class="col-sm-12">
+          <p>Games Library</p>
+          <hr />
+          <br />
+          <!-- Pop up -->
+          <button type="button" class="btn btn-success btn-sm">Add Game</button>
+          <br />
+          <br />
+          <table class="table table-hover">
+            <!-- head -->
+            <thead>
+              <tr>
+                <!-- header cells -->
+                <th scope="col">Title</th>
+                <th scope="col">Genre</th>
+                <th scope="col">Played?</th>
+                <th scope="col">Actions</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>do</td>
+                <td>do</td>
+                <td>do</td>
+                <td>
+                  <div class="btn-group" role="'group">
+                    <button type="button" class="btn btn-info btn-sm">
+                      Update
+                    </button>
+                    <button type="button" class="btn btn-danger btn-sm">
+                      Delete
+                    </button>
+                  </div>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
